1225677d7df27e20f7e50953700a850b88955c6c
Kevin <kevinarmandzakka@gmail.com>
2018-10-11 20:59:29 -0700

    code for new blog post

--- aws-pytorch/install.sh
+++ aws-pytorch/install.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get update && sudo apt-get -y upgrade
 
-# python3 misc
+# python
 sudo apt-get install unzip
 sudo apt-get --assume-yes install python3-tk
 sudo apt-get --assume-yes install python3-pip
@@ -19,9 +19,9 @@ mkdir envs
 cd envs
 virtualenv --system-site-packages deepL
 
-# install pytorch
+# pytorch
 source ~/envs/deepL/bin/activate
-pip install http://download.pytorch.org/whl/cu80/torch-0.4.0-cp35-cp35m-linux_x86_64.whl
+pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp35-cp35m-linux_x86_64.whl
 pip install torchvision tqdm
 
-echo "Reboot required."
+sudo reboot
